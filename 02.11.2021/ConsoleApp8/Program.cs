b7c0af4fe5bdf2f66ff840f2c70cfda66a873943
sabir <tu201806274@code.edu.az>
2021-11-02 00:30:28 +0300

    task 2,3,4

---
+++ 02.11.2021/ConsoleApp8/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task2
+            // int[] arr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+            //Array.Reverse(arr);
+
+            // for (int i = 0; i < arr.Length; i++)
+            // {
+
+            //     Console.WriteLine(arr[i]);
+            // }
+            #endregion
+
+            #region task3
+            //int[] arr1 = { 1, 7, 5 };
+            //int[] arr2 = { 2, 4, 3 };
+            //int[] arr3 = new int[arr1.Length + arr2.Length];
+            //Array.Copy(arr1, arr3, arr1.Length);
+            //Array.Copy(arr2, 0, arr3, arr1.Length, arr2.Length);
+
+            //foreach (var item in arr3)
+            //{
+            //    //Console.WriteLine(item);
+
+            //}
+
+            //Array.Sort(arr3);
+            //Array.Reverse(arr3);
+
+            //foreach (var item in arr3)
+            //{
+            //    //Console.WriteLine(item);
+
+            //}
+
+            //int max = arr3.Max();
+            //    Console.WriteLine($"Max: {max}");
+            //int min = arr3.Min();
+            //Console.WriteLine($"Min: {min}");
+            #endregion
+
+            //int[] arry = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            //int a = arry.Length - 1;
+            //for (int i = 0; i < arry.Length; i++)
+            //{
+                
+            //    if (a%2==0)
+            //    {
+            //        Console.WriteLine(arry[i]);
+            //    }   
+
+
+            //}
+
+        }
+
+
+
+    }
+}
