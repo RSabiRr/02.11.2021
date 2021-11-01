4b46484a63c2b8227f5b047d64a227f1f7c56ca4
sabir <tu201806274@code.edu.az>
2021-11-02 00:27:24 +0300

    task1

---
+++ 02.11.2021/ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Rasim is a masculine given name of Arabic origin meaning \"planner\" ,\"architect\"";
+
+
+            if (text.Length>50)
+            {
+                Console.WriteLine(text.Substring(0, 50)+"...");
+            }
+            else
+            {
+                Console.WriteLine(text);
+
+            }
+        }
+    }
+}
