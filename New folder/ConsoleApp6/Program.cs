d3ac28f10d480cc49122f8f88047fefd65592422
Aliyev <tu201806274@aztu.local>
2021-11-02 10:23:53 +0400

    string task

---
+++ New folder/ConsoleApp6/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region task1
+            //string text = "Hello p121!";
+
+            //Console.WriteLine($"The string you entered is: {text}");
+            #endregion
+            #region task 2
+            //    string text = "Hello p121";
+
+            //    Console.WriteLine($"Length of the string is:  {text1(text)}");
+
+
+            //}
+            //static int text1(string text)
+            //{
+            //    int count = 0;
+            //    foreach (char item in text)
+            //    {
+            //        count++;
+            //    }
+            //    return count;
+            //}
+            #endregion
+            #region task3
+            //string text = "Welcome,ca !";
+
+            //Console.WriteLine(revers2(text));
+
+            //static string revers2(string text)
+            //{
+            //    string revers = "";
+            //    char[] text1 = text.ToCharArray();
+
+
+            //    for (int i = text.Length - 1; i >= 0; i--)
+            //    {
+            //        revers = revers + text1[i]+" ";
+            //    }
+            //    return revers;
+            //}
+            #endregion
+            #region task 4
+            //string text = "This is Code Academy";
+            //int l, say;
+
+            //total(text);
+
+            //static int total(string text)
+            //{
+            //    int l=0, say=1 ;
+
+            //    while (l <= text.Length - 1)
+            //    {
+            //        if (text[l] == ' ' && text[l + 1] != ' ')
+            //        {
+            //            say++;
+            //        }
+            //        l++;
+            //    }
+            //    Console.WriteLine($"sayi: {say}");
+            //    return say;
+            //}
+            #endregion
+
+
+        }
+    }
+}
